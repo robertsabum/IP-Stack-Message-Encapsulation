6148e1a333a27e36716bee6bd974d584cdd01a56
Robert Sabum <rjsabum@noctrl.edu>
2023-04-26 18:42:59 -0500

    save

--- Layers/Application/HTTPResponse.h
+++ Layers/Application/HTTPResponse.h
@@ -7,20 +7,6 @@
 
 using namespace std;
 
-vector<string> split(string str, char delimiter)
-{
-  vector<string> internal;
-  stringstream ss(str);
-  string tok;
-
-  while (getline(ss, tok, delimiter))
-  {
-    internal.push_back(tok);
-  }
-
-  return internal;
-}
-
 class HTTPResponse
 {
 private:
